12b400ff7e44cf8eec0a75311e6e3e9916c6ed12
MrArturtleNSCDS <31484951+MrArturtleNSCDS@users.noreply.github.com>
2020-01-08 00:02:52 -0600

    Add files via upload

--- functions/functions.js
+++ functions/functions.js
@@ -1,12 +1,6 @@
-/*eslint-env jquery */
 $('#myButton').click(
     function(){
-        var x = $('#lowNum').val();
-        var numX = parseInt(x);
-        
-        if(numX % 2==0)
-            $('#result').text("even");
-        else
-            $('#result').text("odd");
+        var userName = $('#infoBox').val();
+        $('#result').text("Hello!!!" + userName);
     }
 );
