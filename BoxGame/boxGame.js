130e4d89cb022cca573183211ef411129405f81a
MrArturtleNSCDS <agarcia@nscds.org>
2020-02-21 15:59:11 +0000

    finished board

--- BoxGame/boxGame.js
+++ BoxGame/boxGame.js
@@ -1,5 +1,19 @@
 console.info("javascript loaded");
+var gameBoard = $("#gameBoard");
 
-for(var r=0; r<30; r++){
-    $("#gameBoard").append("<div class='dot'></div>");
+
+for(var r=0; r<20; r++){
+    if(r%2==0)
+        for(var c=0; c<10; c++){
+            gameBoard.append("<div class='dot'></div>");
+            if(c<9)
+                gameBoard.append("<div class='line hLine'></div>");
+        }
+    else
+        if(r<19)
+            for(var c=0; c<10; c++){
+                gameBoard.append("<div class='line vLine'></div>");
+                if(c<9)
+                    gameBoard.append("<div class='box'></div>");
+            }
 }
